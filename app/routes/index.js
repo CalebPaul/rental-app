3d2da8ea94bac1261213095af0ebd80dec8ca3a1
Caleb Paul and Josh Huffman <caleb@>
2016-10-24 10:12:21 -0700

    add show and hide functionality for images

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,4 +5,11 @@ export default Ember.Route.extend({
   model() { //model hook
     return this.store.findAll('rental');
   },
+
+  actions: {
+    destroyRental(rental) {
+      rental.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
