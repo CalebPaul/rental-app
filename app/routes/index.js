ed0a739e405b8701997a93dd63ab682118094d5d
Caleb Paul and Josh Huffman <josh@>
2016-10-24 10:40:25 -0700

    add save function

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,17 @@
 import Ember from 'ember';
 
-
 export default Ember.Route.extend({
   model() { //model hook
     return this.store.findAll('rental');
   },
 
   actions: {
+    saveRental3(params) {
+      var newRental = this.store.createRecord('rental', params);
+      newRental.save();
+      this.transitionTo('index');
+    },
+
     destroyRental(rental) {
       rental.destroyRecord();
       this.transitionTo('index');
